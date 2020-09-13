b1889258c30b513a0722fbc79542f3e2d0de9022
Victor Romero <romeroa.victor@yahoo.com>
2020-09-13 20:08:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -16,7 +16,7 @@ def prompt_user
   puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
+def get_user_input(users_input)
   users_input=users_input.gets.chomp
 
   # code #get_user_input here
